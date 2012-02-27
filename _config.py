a3e18d34f2e278b23a7318d62674fba7da5aef00
Bert JW Regeer <bertjw@regeer.org>
2012-02-26 23:13:13 -0700

    Add all social links
    This adds all of the social networking links for my personal profiles on
    various sites.
    
    Not yet sure if this will stick around.

--- _config.py
+++ _config.py
@@ -54,3 +54,15 @@ blog.custom_index = False
 
 # Add another "filter"
 blog.post_default_filters['md'] = 'markdown'
+
+social = [
+    {'title': "Twitter",    'alt': "Twitter: @bertjwregeer",    'url': "http://twitter.com/bertjwregeer"},
+    {'title': "Flickr",     'alt': "Flickr: xistence",          'url': "http://www.flickr.com/photos/xistence/"},
+    {'title': "Facebook",   'alt': "Facebook: xistence",        'url': "http://facebook.com/xistence/"},
+    {'title': "LinkedIn",   'alt': "LinkedIn: bertjwregeer",    'url': "http://www.linkedin.com/in/bertjwregeer"},
+    {'title': "Hacker News",'alt': "Hacker News: X-Istence",    'url': "http://news.ycombinator.com/user?id=X-Istence" },
+    {'title': "BitBucket",  'alt': "Bitbucket: xistence",       'url': "http://code.bertjwregeer.com/" },
+    {'title': "Github",     'alt': "Github: bertjwregeer",      'url': "https://github.com/bertjwregeer" },
+    {'title': "Google+",    'alt': "Google+: Bert JW Regeer",   'url': "https://plus.google.com/113979921696834019350" },
+]
+
